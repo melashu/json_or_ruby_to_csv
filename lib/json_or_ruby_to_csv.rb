74fb5f4ad549d3e6ac3e37a29090e17de6d25568
melashu <meshu102@outlook.com>
2023-05-21 17:00:40 +0300

    Add main implemention

---
+++ lib/json_or_ruby_to_csv.rb
@@ -0,0 +1,63 @@
+require_relative './json_or_ruby_to_csv/helper'
+
+module JsonOrRubyToCsv
+  include Helper
+  def array_or_hash_to_csv(data)
+
+    case data
+
+    when Array
+      begin
+        new_data = data.map(&:to_h)
+        convert_array_to_csv(new_data)
+      rescue StandardError
+        'Invalid format!'
+      end
+    when Hash
+      convert_hash_to_csv(data)
+    else
+      'Invalid format!'
+    end
+  end
+
+  
+
+
+
+  def activerecord_to_csv(data)
+
+     if data.respond_to?(:each)
+
+      relation_to_array(data)
+
+     else
+
+      object_to_hash(data)
+
+     end
+
+  end
+
+end
+
+
+
+# class Dummy
+
+#   include JsonOrRubyToCsv
+
+
+
+#   def show(data)
+
+#     array_or_hash_to_csv(data)
+
+#   end
+
+# end
+
+
+
+# d = Dummy.new
+
+# p d.show({name:'Meshu'})
