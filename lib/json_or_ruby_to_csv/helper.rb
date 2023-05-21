d63b999e86000bbbbed2708ab429b36544e23d6e
melashu <meshu102@outlook.com>
2023-05-21 17:00:59 +0300

    Add helper module

---
+++ lib/json_or_ruby_to_csv/helper.rb
@@ -0,0 +1,35 @@
+module Helper
+  def name; end
+
+  def convert_array_to_csv(data)
+    csv_keys = data[0].keys.join(',')
+
+    csv_file = data.map do |ele|
+      ele.values.join(',')
+    end.join("\n")
+
+    "#{csv_keys}\n#{csv_file}"
+  end
+
+  def convert_hash_to_csv(data)
+    csv_keys = data.keys.join(',')
+
+    csv_values = data.values.join(',')
+
+    "#{csv_keys}\n#{csv_values}"
+  end
+
+  def relation_to_array(data)
+    array_value = data.map(&:attributes)
+
+    convert_array_to_csv(array_value)
+  rescue StandardError
+    'Invalid format!'
+  end
+
+  def object_to_hash(data)
+    convert_hash_to_csv(data.attributes)
+  rescue StandardError
+    'Invalid format!'
+  end
+end
